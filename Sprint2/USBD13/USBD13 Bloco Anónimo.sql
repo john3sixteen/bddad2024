7cf25dd0721a75abde095149e1b9f615486d1fa7
1201532-Rodrigo-Silva <1201532@isep.ipp.pt>
2024-11-22 19:40:12 +0000

    USBD13 Bloco anonimo

--- Sprint2/USBD13/USBD13 Bloco Anónimo.sql
+++ Sprint2/USBD13/USBD13 Bloco Anónimo.sql
@@ -1,40 +1,28 @@
---Test
-
 DECLARE 
-		
-	myCursor 				SYS_REFCURSOR;
-	myCursor2 				SYS_REFCURSOR;
-	subProduct				Part.Id%type;
-	partName				Part.Name%type;
-	operationId				Operation.Id%type;
-	operationDescription	OperationType.Description%type;
-	partInputName			Part.Name%type
-	partOutputName			Part.Name%type
-	workstationTypeId		WorkstationType.Id%type;
-	workstationTypeName		WorkstationType.Name%type;
+    myCursor               SYS_REFCURSOR;
+    partName               Part.Name%type;
+    operationId           Operation.Id%type;
+    operationDescription  OperationType.Description%type;
+    partInputName         Part.Name%type;
+    partOutputName        Part.Name%type;
+    workstationTypeId     WorkstationType.Id%type;
+    workstationTypeName   WorkstationType.Name%type;
 
 BEGIN
-	myCursor := fncGetOperationList('3');
-	LOOP
-		FETCH myCursor INTO partName, operationId, operationDescription, workstationTypeId, workstationTypeName;
-		EXIT WHEN myCursor%NOTFOUND;
-		dbms_output.put_line('Product: ' || partName || ' | Operation: ' || operationId || '-' || operationDescription || ' | Input: ' || partInputName || ' | Output: ' || partOutputName ||' | Workstation Type: ' || workstationTypeId || '-' || workstationTypeName);
-		myCursor2 := fncIsProduct(partName);
-		IF myCursor2%ROWCOUNT > 0 NULL THEN
-			FETCH myCursor2 INTO subProduct;
-			myCursor2 := fncGetOperationList(subProduct);
-			LOOP
-				FETCH myCursor2 INTO partName, operationId, operationDescription, workstationTypeId, workstationTypeName;
-				EXIT WHEN myCursor2%NOTFOUND;
-				dbms_output.put_line('Product: ' || partName || ' | Operation: ' || operationId || '-' || operationDescription || ' | Input: ' || partInputName || ' | Output: ' || partOutputName ||' | Workstation Type: ' || workstationTypeId || '-' || workstationTypeName);
-			END LOOP;
-		END IF
-	END LOOP;
-	IF myCursor%ROWCOUNT = 0 THEN
-		dbms_output.put_line('Product does not have Bill of Operations.');
-	END IF;
+    myCursor := fncGetOperationList('3');
+    LOOP
+        FETCH myCursor INTO partName, operationId, operationDescription, partInputName, partOutputName, workstationTypeId, workstationTypeName;
+        EXIT WHEN myCursor%NOTFOUND;
+        dbms_output.put_line('Product: ' || partName || ' | Operation: ' || operationId || ' -> ' || operationDescription || 
+                             ' | Input: ' || partInputName || ' | Output: ' || partOutputName || ' | Workstation Type: ' || 
+                             workstationTypeId || ' -> ' || workstationTypeName);
+    END LOOP;
+    
+    IF myCursor%ROWCOUNT = 0 THEN
+        dbms_output.put_line('Product does not have Bill of Operations.');
+    END IF;
 
 EXCEPTION
-	WHEN NO_DATA_FOUND THEN
-		dbms_output.put_line('No product was found with that Id.');
+    WHEN NO_DATA_FOUND THEN
+        dbms_output.put_line('No product was found with that Id.');
 END;
