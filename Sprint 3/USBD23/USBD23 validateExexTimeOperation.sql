b2dba71343590c45e0e61746830880229fd119b0
1201532-Rodrigo-Silva <1201532@isep.ipp.pt>
2024-12-13 19:20:40 +0000

    Update USBD23 validateExexTimeOperation.sql

--- Sprint 3/USBD23/USBD23 validateExexTimeOperation.sql
+++ Sprint 3/USBD23/USBD23 validateExexTimeOperation.sql
@@ -2,17 +2,16 @@ CREATE OR REPLACE TRIGGER validateExecTimeOperation
     BEFORE INSERT OR UPDATE ON Operation
     FOR EACH ROW
 
-    DECLARE maximumTimes SYS_REFCURSOR;
+    DECLARE maximumTimes NUMBER;
     ERROR EXCEPTION;
 
 BEGIN
-    OPEN maximumTimes FOR
-        SELECT OperationType_WorkstationType.MaximumTime
-            FROM OperationType_WorkstationType
-            WHERE OperationType_WorkstationType.OperationTypeId = :new.OperationTypeId 
-            AND OperationType_WorkstationType.MaximumTime < :new.ExpectedTime;
+    SELECT COUNT(*) INTO maximumTimes
+        FROM OperationType_WorkstationType
+        WHERE OperationType_WorkstationType.OperationTypeId = :new.OperationTypeId 
+        AND OperationType_WorkstationType.MaximumTime < :new.ExpectedTime;
 
-    IF (maximumTimes%FOUND) THEN
+    IF (maximumTimes > 0) THEN
         RAISE ERROR;
     END IF;
 
