ed42fd1921977475a041b9d09ade818011d6d406
Francisco Oliveira (FDSO) <1180862@isep.ipp.pt>
2025-01-04 12:50:37 +0000

    [USBD23] - Correction of the values on the tests of USBD23

--- Sprint 3/USBD23/USBD23 Tests.sql
+++ Sprint 3/USBD23/USBD23 Tests.sql
@@ -1,14 +1,14 @@
 --1st test - The Operation expected time is bigger than one of its OperationType_WorkstationType - Should give an error
 INSERT INTO Operation(Id,OperationTypeId,BOOId, NextOp,ExpectedTime) VALUES(101, 5647, '1', 103, 13);
 --2nd test - The Operation expected time is lower than all its OperationType_WorkstationType - Should be successful
-INSERT INTO Operation(Id,OperationTypeId,BOOId, NextOp,ExpectedTime) VALUES(102, 5647, '1', 103, 12);
---3rd test - The Operation expected time is the same size as one of its OperationType_WorkstationType - Should be successful
+INSERT INTO Operation(Id,OperationTypeId,BOOId, NextOp,ExpectedTime) VALUES(102, 5647, '1', 103, 11);
+--3rd test - The Operation expected time is the same time as the OperationType_WorkstationType of the operation with the least expected time - Should be successful
 UPDATE Operation
-SET ExpectedTime = 13
+SET ExpectedTime = 12
 WHERE Id = 102
 --4th test - The Operation expected time is bigger than one of its OperationType_WorkstationType - Should give an error
 UPDATE Operation
-SET ExpectedTime = 16
+SET ExpectedTime = 14
 WHERE Id = 102
 --5th test - The OperationType_WorkstationType Maximum time is bigger than the expected time of all its operations - Should be successful
 UPDATE OperationType_WorkstationType
